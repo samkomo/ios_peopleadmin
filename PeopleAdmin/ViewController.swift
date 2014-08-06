eca9b7d95fe0cd7d5ce0c1ab58b778165f610447
Santiago Hurtado <shurtado@wake.local>
2014-08-05 23:19:32 -0500

    Initial Commit

---
+++ PeopleAdmin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PeopleAdmin
+//
+//  Created by Santiago Hurtado on 8/5/14.
+//  Copyright (c) 2014 Advance Teknologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
